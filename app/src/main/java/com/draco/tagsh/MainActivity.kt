59b54152d1932424847db0adf987555c7c00f973
tytydraco <tylernij@gmail.com>
2020-03-21 20:07:09 -0700

    Add initial support for QR and barcode scanning
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/tagsh/MainActivity.kt
+++ app/src/main/java/com/draco/tagsh/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.draco.tagsh
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -11,6 +12,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 import java.util.concurrent.atomic.AtomicBoolean
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +20,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nfc: Nfc
 
     /* Internal constants */
-    private val tagScriptName = "script.sh"
+    private val scriptName = "script.sh"
     private val requestCodeSelectScript = 1
 
     /* Internal variables */
-    private lateinit var tagScriptPath: String
+    private lateinit var scriptPath: String
     private lateinit var readyToFlashDialog: AlertDialog
     private var pendingScriptBytes = byteArrayOf()
     private var currentlyExecuting = AtomicBoolean()
@@ -70,16 +72,15 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(chooserIntent, requestCodeSelectScript)
     }
 
-    /* Execute whatever script is on NFC tag */
-    private fun executeScriptFromTag(intent: Intent) {
-        /* Read contents as compressed bytes */
-        val nfcContent = nfc.readBytes(intent) ?: return
-
-        /* Write contents to local file */
-        val fileOutputStream = openFileOutput(tagScriptName, Context.MODE_PRIVATE)
-        fileOutputStream.write(nfcContent)
+    /* Write our script using the script byte array */
+    private fun writeScript(bytes: ByteArray) {
+        val fileOutputStream = openFileOutput(scriptName, Context.MODE_PRIVATE)
+        fileOutputStream.write(bytes)
         fileOutputStream.close()
+    }
 
+    /* Execute our saved script */
+    private fun executeScript() {
         /* Clear any existing output */
         outputView.text = ""
 
@@ -87,7 +88,7 @@ class MainActivity : AppCompatActivity() {
         Thread {
             currentlyExecuting.set(true)
             /* Execute shell script */
-            val processBuilder = ProcessBuilder("sh", tagScriptPath)
+            val processBuilder = ProcessBuilder("sh", scriptPath)
                 .redirectErrorStream(true)
                 .start()
 
@@ -120,8 +121,16 @@ class MainActivity : AppCompatActivity() {
             currentlyExecuting.set(false)
 
             /* Delete script for security reasons */
-            deleteFile(tagScriptName)
+            deleteFile(scriptName)
         }.start()
+    }
+
+    /* Execute whatever script is on NFC tag */
+    private fun executeScriptFromTag(intent: Intent) {
+        /* Read contents as compressed bytes */
+        val nfcContent = nfc.readBytes(intent) ?: return
+        writeScript(nfcContent)
+        executeScript()
     }
 
     /* On activity creation */
@@ -132,7 +141,7 @@ class MainActivity : AppCompatActivity() {
         /* Lateinit setup */
         scrollView = findViewById(R.id.scrollView)
         outputView = findViewById(R.id.output)
-        tagScriptPath = "${filesDir}/${tagScriptName}"
+        scriptPath = "${filesDir}/${scriptName}"
 
         /* Setup ready to flash dialog */
         readyToFlashDialog = AlertDialog.Builder(this)
@@ -168,9 +177,18 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == requestCodeSelectScript && resultCode == RESULT_OK)
+        if (resultCode != Activity.RESULT_OK)
+            return
+
+        if (requestCode == requestCodeSelectScript)
             if (data != null)
                 loadScriptFromStorage(data.data!!)
+
+        val scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (scanResult != null && scanResult.contents != null) {
+            writeScript(scanResult.contents.toByteArray())
+            executeScript()
+        }
     }
 
     /* On toolbar menu item click */
@@ -179,6 +197,15 @@ class MainActivity : AppCompatActivity() {
             R.id.flash -> {
                 /* Ask user to select script from storage */
                 selectScriptFromStorage()
+            }
+
+            R.id.scan -> {
+                /* Initiate QR or barcode scan */
+                IntentIntegrator(this)
+                    .setPrompt("")
+                    .setBeepEnabled(false)
+                    .setOrientationLocked(false)
+                    .initiateScan()
             }
         }
 
