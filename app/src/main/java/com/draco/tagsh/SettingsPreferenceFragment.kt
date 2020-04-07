0df135d6dfa4509ceb04dd34cdfcba32eadaef3f
tytydraco <tylernij@gmail.com>
2020-04-06 22:49:55 -0700

    Add view store page button
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/tagsh/SettingsPreferenceFragment.kt
+++ app/src/main/java/com/draco/tagsh/SettingsPreferenceFragment.kt
@@ -15,6 +15,14 @@ class SettingsPreferenceFragment : PreferenceFragmentCompat() {
     /* Process preference clicks */
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
         if (preference != null) when (preference.key) {
+            /* Take user to the Google Play details page */
+            "viewStore" -> {
+                val uri = Uri.parse("market://details?id=" + requireContext().packageName)
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+
+                startActivity(intent)
+            }
+
             /* Take user to the source code */
             "github" -> {
                 val intent = Intent(Intent.ACTION_VIEW)
