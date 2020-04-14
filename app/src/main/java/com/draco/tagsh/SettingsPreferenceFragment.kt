5198510238b2abed9391754bc866679f74c49035
tytydraco <tylernij@gmail.com>
2020-04-14 16:47:41 -0700

    Add storage request feature
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/tagsh/SettingsPreferenceFragment.kt
+++ app/src/main/java/com/draco/tagsh/SettingsPreferenceFragment.kt
@@ -1,12 +1,18 @@
 package com.draco.tagsh
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsPreferenceFragment : PreferenceFragmentCompat() {
+    /* Request code for when the user wants to request storage access */
+    private val requestCodeStoragePermissions = 0
+
     /* Setup our preference screen */
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference_settings, rootKey)
@@ -15,6 +21,15 @@ class SettingsPreferenceFragment : PreferenceFragmentCompat() {
     /* Process preference clicks */
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
         if (preference != null) when (preference.key) {
+            /* Grant storage access using legacy Android Q API */
+            "grantStorageAccess" -> {
+                if (activity != null)
+                    ActivityCompat.requestPermissions(activity as Activity,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_SECURE_SETTINGS),
+                        requestCodeStoragePermissions);
+            }
+
             /* Take user to the Google Play details page */
             "viewStore" -> {
                 val uri = Uri.parse("market://details?id=" + requireContext().packageName)
