f2a8090e08c383ec0f59787b00fb5c476054054a
tytydraco <tylernij@gmail.com>
2020-05-25 13:37:10 -0700

    Allow clearweb traffic
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/tagsh/WebViewActivity.kt
+++ app/src/main/java/com/draco/tagsh/WebViewActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class WebViewActivity : AppCompatActivity() {
